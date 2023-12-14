d2d260f0887f9e95e6ef92cd24de81757cff96dc
Alistair Robinson <alistair.robinson@gmail.com>
2023-12-14 23:01:22 +0300

    Change JS to use exported/imported modules

---
+++ public/scripts/admin-main.js
@@ -0,0 +1,123 @@
+import { Article } from "./article-properties.js"
+
+/*
+  Entry point for the article properties form
+  state management
+*/
+window.addEventListener("doSetupArticle", ()=>{
+  /*
+    Triggered by HTMX via header from server:
+    Header "HX-Trigger-After-Settle" = "doSetupArticle"
+    sent from article_properties response (only)
+  */
+  setupArticle();
+})
+
+
+function setupArticle() {
+
+  const a = Article({
+    articleId:                document.getElementById("inpArticleID").value,
+    isDraft:                  document.getElementById("inpDraft").value,
+    alertUnpublishedChanges:  document.getElementById("unpublished-changes"),
+    alertArticleStatus:       document.getElementById("article-status"),
+    confAfterRequest:         document.getElementById("confirm-published"),
+    btRevert:                 document.getElementById("revert"),
+    btPublish:                document.getElementById("publish"),
+    btUnpublish:              document.getElementById("unpublish"),
+    alertPublishErrors:       document.getElementById("error-message"),
+    inps:                     document.querySelectorAll("input, select"),
+    btPublishAction:          document.getElementById("publishAction")
+  })
+
+  a.events.mainLoad();
+
+
+  /*
+    AUTOSAVE
+  */
+
+  const autosaveEvents = ["input", "change"];
+  const autoSaveElementIDs = [
+    "inpSlug",
+    "inpDate",
+    "inpTitle",
+    "inpDate",
+    "inpTags",
+    "selImageClass"
+  ];
+  const syncedPairs = {inpTitle: "inpSlug"};
+
+  autosaveEvents.forEach(eventType => {
+    document.body.addEventListener(eventType, ev => {
+
+      // Special case for auto-slugification
+      if(ev.target.id==="inpTitle"){
+        document.getElementById("inpSlug").value = slugify(ev.target.value);
+      }
+
+      // On to the autosave
+      if (autoSaveElementIDs.includes(ev.target.id)) {
+        a.events.autosave(ev.target)
+        /*
+          When key (property name) of syncedPairs is saved,
+          value gets saved too (not vice versa)
+        */
+        if (syncedPairs.hasOwnProperty(ev.target.id)) {
+          a.events.autosave(document.getElementById(syncedPairs[ev.target.id]))
+        }
+
+      }
+    })
+  })
+
+
+  /*
+    FORM SUBMISSION RESPONSE
+  */
+
+  document.body.addEventListener('htmx:afterRequest', ev => {
+    const myTarget = ev.detail.target;
+    switch (myTarget.id) {
+      case "publish":
+        let res = JSON.parse(ev.detail.xhr.response);
+        let errors = false;
+        for(const inp of res.validation_results) {
+          if (inp.hasOwnProperty("error")) {
+            errors = true;
+          }
+        }
+        if(errors) {
+          a.events.receivePublishError(res.validation_results)
+        } else {
+          a.events.publish()
+        }
+        break;
+      case "unpublish":
+        a.events.unpublish()
+        break;
+    }
+  });
+
+  //const filepicker = document.getElementById("inpMainImage");
+  //filepicker.addEventListener("change", (event) => {
+  //  const files = event.target.files;
+  //  filepicker.value = files[0].name;
+  //});
+
+} // setupArticle()
+
+
+/*
+  Helper function
+*/
+export function slugify(str) {
+  return String(str)
+  .normalize('NFKD')
+  .replace(/[\u0300-\u036f]/g, '')
+  .trim()
+  .toLowerCase()
+  .replace(/[^a-z0-9 -]/g, '')
+  .replace(/\s+/g, '-')
+  .replace(/-+/g, '-');
+}
--- public/scripts/article-properties.js
+++ public/scripts/article-properties.js
@@ -1,21 +1,21 @@
-function createArticle(params) {
+export const Article = function(params) {
 
   /*
     Factory function for state management in article
     properties form: templates/admin/article_properties.html
   */
 
-  let articleId = params.articleId;
+  const articleId = params.articleId;
+  const alertUnpublishedChanges = params.alertUnpublishedChanges;
+  const alertArticleStatus = params.alertArticleStatus;
+  const confAfterRequest = params.confAfterRequest;
+  const btRevert = params.btRevert;
+  const btPublish = params.btPublish;
+  const btUnpublish = params.btUnpublish;
+  const inputs = params.inps;
+  const btPublishAction = params.btPublishAction;
+  const alertPublishErrors = params.alertPublishErrors
   let isDraft = params.isDraft;
-  let alertUnpublishedChanges = params.alertUnpublishedChanges;
-  let alertArticleStatus = params.alertArticleStatus;
-  let confAfterRequest = params.confAfterRequest;
-  let btRevert = params.btRevert;
-  let btPublish = params.btPublish;
-  let btUnpublish = params.btUnpublish;
-  let inputs = params.inps;
-  let btPublishAction = params.btPublishAction;
-  let alertPublishErrors = params.alertPublishErrors
   let dataSource = getDataSource();
 
   function getDataSource() {
@@ -41,7 +41,7 @@ function createArticle(params) {
     return dSrc;
   }
 
-  states = {
+  const states = {
     published:{
       signalling:{
         UIState:() => {
@@ -112,11 +112,12 @@ function createArticle(params) {
     }
   }
 
-  events = {
+  const events = {
     /*
       These determine the current state
     */
     mainLoad:() => {
+      let currentState = {};
       if(parseInt(isDraft) === 0){
         if(dataSource === "localStorage"){
           currentState = states.published.changes;
@@ -129,6 +130,8 @@ function createArticle(params) {
       currentState.UIState();
     },
     autosave:(inpEl) => {
+      let currentState = {};
+
       const SHOW_SIGNAL_FOR = 2000;
       const DO_AUTOSAVE_AFTER = 1500; // Following last input/change event
 
@@ -158,10 +161,11 @@ function createArticle(params) {
         Remove the localStorage items
         for the current article
       */
+      let currentState = {};
       let storageKeyPrefix = 'article_' + articleId;
-      localStorage.removeItem(storageKeyPrefix)
+      localStorage.removeItem(storageKeyPrefix);
       for(const input of inputs){
-        let storageKey = storageKeyPrefix + "_" + input.name
+        let storageKey = storageKeyPrefix + "_" + input.name;
         localStorage.removeItem(storageKey);
       }
       dataSource = "db";
@@ -185,7 +189,8 @@ function createArticle(params) {
         Set to signalling state for a couple
         of seconds
       */
-      states.draft.signalling.UIState()
+      let currentState = states.draft;
+      currentState.signalling.UIState();
       setTimeout(() => {
         currentState = states.draft;
         currentState.UIState();
@@ -197,115 +202,9 @@ function createArticle(params) {
       alertPublishErrors.classList.add("hidden");
     },
     receivePublishError:(validation_results) => {
-      currentState = states.publishError;
+      let currentState = states.publishError;
       currentState.UIState(validation_results);
     }
   }
-  return { events }
-}
-
-function setupArticle() {
-
-  const a = createArticle({
-    articleId:                document.getElementById("inpArticleID").value,
-    isDraft:                  document.getElementById("inpDraft").value,
-    alertUnpublishedChanges:  document.getElementById("unpublished-changes"),
-    alertArticleStatus:       document.getElementById("article-status"),
-    confAfterRequest:         document.getElementById("confirm-published"),
-    btRevert:                 document.getElementById("revert"),
-    btPublish:                document.getElementById("publish"),
-    btUnpublish:              document.getElementById("unpublish"),
-    alertPublishErrors:       document.getElementById("error-message"),
-    inps:                     document.querySelectorAll("input, select"),
-    btPublishAction:          document.getElementById("publishAction")
-  })
-
-  a.events.mainLoad();
-
-
-  /*
-    AUTOSAVE
-  */
-
-  const autosaveEvents = ["input", "change"];
-  const autoSaveElementIDs = [
-    "inpSlug",
-    "inpDate",
-    "inpTitle",
-    "inpDate",
-    "inpTags",
-    "selImageClass"
-  ];
-  const syncedPairs = {inpTitle: "inpSlug"};
-
-  autosaveEvents.forEach(eventType => {
-    document.body.addEventListener(eventType, ev => {
-      if (autoSaveElementIDs.includes(ev.target.id)) {
-        a.events.autosave(ev.target)
-        /*
-          When key (property name) of syncedPairs is saved,
-          value gets saved too (not vice versa)
-        */
-        if (syncedPairs.hasOwnProperty(ev.target.id)) {
-          a.events.autosave(document.getElementById(syncedPairs[ev.target.id]))
-        }
-
-      }
-    })
-  })
-
-
-  /*
-    FORM SUBMISSION RESPONSE
-  */
-
-  document.body.addEventListener('htmx:afterRequest', ev => {
-    const myTarget = ev.detail.target;
-    switch (myTarget.id) {
-      case "publish":
-        let res = JSON.parse(ev.detail.xhr.response);
-        errors = false;
-        for(const inp of res.validation_results) {
-          if (inp.hasOwnProperty("error")) {
-            errors = true;
-          }
-        }
-        if(errors) {
-          a.events.receivePublishError(res.validation_results)
-        } else {
-          a.events.publish()
-        }
-        break;
-      case "unpublish":
-        a.events.unpublish()
-        break;
-    }
-  });
-
-  //const filepicker = document.getElementById("inpMainImage");
-  //filepicker.addEventListener("change", (event) => {
-  //  const files = event.target.files;
-  //  filepicker.value = files[0].name;
-  //});
-
-} // setupArticle()
-
-window.addEventListener("doSetupArticle", ()=>{
-  /*
-    Triggered by HTMX via header from server:
-    Header "HX-Trigger-After-Settle" = "doSetupArticle"
-    sent from article_properties response (only)
-  */
-  setupArticle();
-})
-
-function slugify(str) {
-  return String(str)
-  .normalize('NFKD')
-  .replace(/[\u0300-\u036f]/g, '')
-  .trim()
-  .toLowerCase()
-  .replace(/[^a-z0-9 -]/g, '')
-  .replace(/\s+/g, '-')
-  .replace(/-+/g, '-');
+  return { events };
 }
